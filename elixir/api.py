1c05d62554e16f6ce9dadabc82a2c126dd7835d3
Daniil159x <daniil.ru-m@mail.ru>
2025-02-15 23:30:40 +0300

    fix review

--- elixir/api.py
+++ elixir/api.py
@@ -46,7 +46,7 @@ class ApiIdentGetterResource:
         if version == 'latest':
             version = query.query('latest')
 
-        symbol_definitions, symbol_references, symbol_doccomments, peaks = query.query('ident', version, ident, family)
+        symbol_definitions, symbol_references, symbol_doccomments, peeks = query.query('ident', version, ident, family)
 
         resp.status = falcon.HTTP_200
         resp.content_type = falcon.MEDIA_JSON
@@ -54,7 +54,7 @@ class ApiIdentGetterResource:
             'definitions': [sym.__dict__ for sym in symbol_definitions],
             'references': [sym.__dict__ for sym in symbol_references],
             'documentations': [sym.__dict__ for sym in symbol_doccomments],
-            'peeks': peaks
+            'peeks': peeks
         }
 
         query.close()
